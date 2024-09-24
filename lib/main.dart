296ac968c49104ecfab91bdda3493d7d46c560af
ismael <ismaelnunes656@gmail.com>
2024-09-24 11:57:41 -0300

    Tech

---
+++ lib/main.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '7tech',
+          style: TextStyle(color: Colors.white), 
+        ),
+        backgroundColor: Colors.blue[700],
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.white),
+          onPressed: () {
+
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings, color: Colors.white),
+            onPressed: () {
+
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30.0)),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.33,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.blue[800]!,
+                    Colors.blue[400]!,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+                image: DecorationImage(
+                  image: AssetImage('assets/images/imagemB.png'),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(Colors.blue.withOpacity(0.5), BlendMode.dstATop),
+                ),
+              ),
+              child: Center(
+                child: Image.asset(
+                  'assets/images/imagemA.png',
+                  width: 150,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                color: Colors.white,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16.0,
+                  mainAxisSpacing: 16.0,
+                  children: [
+                    ToolCard(title: 'Status de Serviço', icon: Icons.assignment),
+                    ToolCard(title: 'Agendamentos', icon: Icons.calendar_today),
+                    ToolCard(title: 'Contatos', icon: Icons.contacts),
+                    ToolCard(title: 'Recibos e avaliações', icon: Icons.receipt),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue[800],
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.white),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, color: Colors.white),
+            label: 'Pesquisar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications, color: Colors.white),
+            label: 'Notificações',
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        onTap: (index) {
+
+        },
+      ),
+    );
+  }
+}
+
+class ToolCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  const ToolCard({required this.title, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.blue[700]!,
+            Colors.blue[400]!,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: InkWell(
+        onTap: () {
+
+        },
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, size: 60, color: Colors.white),
+              SizedBox(height: 8),
+              Text(
+                title,
+                style: TextStyle(fontSize: 18, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
